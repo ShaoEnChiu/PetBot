aa639569a5299d33f255dacddf1e7e60b1675ea5
az507929Jason <abc105692000@gmail.com>
2017-11-13 15:59:42 +0800

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app = Flask(__name__)
 # 測試app.py有沒有Deploy成功
 @app.route("/", methods=["GET"])
 def index():
-	return "hello world 7 >>> index", 200
+	return "hello world 8 >>> index", 200
 
 # 在Line的Document當中有寫到，webhook URL會透過post request呼叫 https://{urladdress}/callback
 @app.route("/callback", methods=["POST"])
@@ -38,7 +38,7 @@ def processMessage(msg):
 	pat = re.compile(r".*(掰掰).*")
 	mat = pat.findall(msg)
 	if len(mat) == 0:
-		ret.append('朕知道了')
+		ret.append('http://i.imgur.com/xjTET8v.jpg')
 		ret.append('可以退下了')
 	else:
 		ret.append('慢走不送')
